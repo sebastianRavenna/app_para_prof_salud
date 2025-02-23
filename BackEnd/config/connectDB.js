8485e72263f3ba2d6e3a3e7043386d330af0216a
Sebastian Ravenna <sebastian.ravenna@gmail.com>
2025-02-23 11:19:04 -0300

    update connectDB

--- BackEnd/config/connectDB.js
+++ BackEnd/config/connectDB.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-process.loadEnvFile ();
+dotenv.config(); // ✅ Carga las variables de entorno del archivo .env
+/* process.loadEnvFile (); */
 
 const URI_DB = process.env.URI_DB;
 
@@ -9,7 +11,9 @@ const connectDB = async () => {
         await mongoose.connect(URI_DB);
         console.log("Conectado a MongoDB Atlas 🚀", mongoose.connection.name);
     } catch (error) {
-        console.error(error);
+        console.error("Error al conectar a MongoDB:", error);
+        process.exit(1); // 🚨 Cerrar la app si la conexión falla
+        /* console.error(error); */
     }
 };
 
