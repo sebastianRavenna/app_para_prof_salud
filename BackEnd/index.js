cd05211490e7cd02841383b8bae5fa3845bfd0a7
Sebastian Ravenna <sebastian.ravenna@gmail.com>
2025-02-27 09:42:45 -0300

    updated session-express

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -32,7 +32,7 @@ app.use(
     cookie: { 
       secure: true,  
       httpOnly: true, 
-      sameSite: "lax", 
+      sameSite: "none", 
       maxAge: 86400000,
 /*       domain: 'localhost',
       path: '/'
